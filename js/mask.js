9d16a7d147f8065b163be720d4079153dca5d119
liuyangnj <liuyangnj@163.com>
2015-09-02 15:58:36 +0800

    使用对象冒充方法proxy
    使用对象冒充方法proxy，将函数内的this指向mask.functions

--- js/mask.js
+++ js/mask.js
@@ -8,14 +8,14 @@ mask.functions = {
 
      creatMask : function(){
          if(!window.localStorage.isMask){
-            var that=this;
+
              var _mask = $("<div id='maskDiv' class='mask'></div>");
              var _tipsImg = $("<div class='tipsImg' ></div>");
              var _tips = $("<div class='tips'>点击此按钮进入报名页面</div>");
              var _btn = $("<div id='closeBtn' class='closeBtn'><div>我知道了</div></div>");
              _mask.append(_tipsImg).append(_tips).append(_btn);
              $("body").append(_mask).css("overflow","hidden");
-             $("#closeBtn").click(function(){that.closeMask();});
+             $("#closeBtn").click(function(){ $("#closeBtn").click(function(){$.proxy(mask.functions.closeMask(),mask.functions);});});
              window.localStorage.isMask = 1111;
          }
          else{$(".class_month li").removeClass("hou");}
